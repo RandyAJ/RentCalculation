751e8768605032b09d71ceeea1ed746ec28e2ba0
Asota Randy <randy201312@gmail.com>
2025-01-27 01:45:53 +0300

    подключил БД, смапил таблицы,поля

--- src/main/java/org/rent/Main.java
+++ src/main/java/org/rent/Main.java
@@ -1,13 +1,26 @@
 package org.rent;
 
-import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        if(console.nextLine().equals("go")){
-            Record rec = new Record(100.0, 200.0, 300.0, 5.0, 4.0);
-            System.out.println(rec);
-        }
+        // Создаем SessionFactory, используя файл hibernate.cfg.xml
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+        Session session = factory.openSession();
+        session.beginTransaction();
+        Record rec = new Record(28.19, 24.06, 43.4, 1.709, 4.101);
+
+        session.save(rec);
+
+        session.getTransaction().commit();
+
+        System.out.println("Сохранено: " + rec);
+
+        // Закрываем сессию и фабрику
+        session.close();
+        factory.close();
     }
 }
--- src/main/java/org/rent/Record.java
+++ src/main/java/org/rent/Record.java
@@ -1,9 +1,15 @@
 package org.rent;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity // говорит о том что класс является сущностью (Entity) для базы данных (Entity(сущность) - объект бизнес логики, обычно сущность представляет собой таблицу в реляционной базе данных, и каждый экземпляр сущности соответствует строке в этой таблице)
+@Table(name = "records") // указывает имя таблицы, соответствующей этой сущности
 public class Record {
-    Record(
+
+    // конструктор для создания записи
+    public Record(
             Double t1, Double t2, Double t3,
             Double hot, Double cold
     ){
@@ -15,29 +21,42 @@ public class Record {
         this.cold = cold;
         this.wd = setWd(hot, cold);
 
-        this.prevRecord = setPrevRecord();
+        this.prevRecordId = setPrevRecordId();
         this.dateRecord = setDateRecord();
         this.total = setTotal();
     }
 
+    @Id // Указывает, что это поле является первичным ключом
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Автоматическая генерация ID
+    private Long id;
+
+    @Column(name = "t1", nullable = false) // Поле будет колонкой t1 в таблице
     Double t1;
+    @Column(name = "t2", nullable = false)
     Double t2;
+    @Column(name = "t3", nullable = false)
     Double t3;
 
+    @Column(name = "hot", nullable = false)
     Double hot;
+    @Column(name = "cold", nullable = false)
     Double cold;
+    @Column(name = "wd", nullable = false)
     Double wd; // water drainage (водоотвод)
 
-    private Record prevRecord;
-    private LocalDate dateRecord;
-    private Double total;
+    //@Column(name = "t2", nullable = false)
+    private final Long prevRecordId; // Если Record prevRecord, то риск StackOverflow. при подгрузке объекта из БД может получаться здоровая портянка с цепочкой объектов
+    @Column(name = "dateRecord", nullable = false)
+    private final LocalDate dateRecord;
+    private final Double total;
 
     private Double setWd(Double hot, Double cold){
-        return hot; // TODO
+        return hot + cold;
     }
 
-    private Record setPrevRecord(){
-        //return new Record(1.0, 1.0, 1.0,1.0,1.0); // прикольный кейс. тут будет бесконечный вызов конструктора и java.lang.StackOverflowError
+    private Long setPrevRecordId(){
+        // link db (postgresql), adding and using dependencie hibenate and HQL (something like Record.createCriteria OR SELECT FROM RECORD)
+        // return new Record(1.0, 1.0, 1.0, 1.0, 1.0); // прикольный кейс хоть и очевидный. тут будет бесконечный вызов конструктора и java.lang.StackOverflowError
         return null; // TODO
     }
 
@@ -46,11 +65,24 @@ public class Record {
     }
 
     private Double setTotal(){
-        return 52.0; // TODO
+        Double t1Result = this.t1 * 8.94;
+        Double t2Result = this.t2 * 3.02;
+        Double t3Result = this.t3 * 6.15;
+
+        Double hotResult = this.hot * 272.14;
+        Double coldResult = this.cold * 59.80;
+
+        Double wdResult = this.wd * 45.91;
+
+        return t1Result + t2Result + t3Result + hotResult + coldResult + wdResult;
     }
 
     @Override
     public String toString() {
-        return "Запись от " + this.dateRecord + ". ТоталОчка: " + this.total;
+        return "Record(" +
+                "id=" + id +
+                ", dateRecord=" + dateRecord +
+                ", total=" + total +
+                ")";
     }
 }
